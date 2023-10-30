02e708f325aab1f55d1d3e583ad655cf6127fd84
rrobaa <robaa@hetton.se>
2023-10-30 09:44:57 +0100

    Ping test 2

--- ping.py
+++ ping.py
@@ -5,7 +5,8 @@ devices = ['192.168.1.1', '192.168.1.2', '192.168.1.3']
 
 def ping_device(device):
     # Use the ping command with count 1 (one packet)
-    result = subprocess.call(['ping', '-c', '1', device])
+    with open('/dev/null', 'w') as DEVNULL:
+        result = subprocess.call(['ping', '-c', '1', device], stdout=DEVNULL, stderr=DEVNULL)
     
     if result == 0:
         print(f"{device} is reachable")
