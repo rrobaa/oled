40f2b5e24042e64bd8d0a6b768cf77a5b07aed61
rrobaa <robaa@hetton.se>
2023-10-31 13:23:03 +0100

    test

--- oled.py
+++ oled.py
@@ -139,7 +139,7 @@ device_thread.start()
 def network_display():
     draw.rectangle((0, 0, width, height), outline=0, fill=0)
     draw.text((0, 0), f"WAN: Internet {device_statuses['WAN']}", font=font_others, fill=255)
-    draw.text((0, 12), f"FR01: {device_statuses['Router']}", font=font_others, fill=255)
+    draw.text((0, 12), f"MFR: {device_statuses['Router']}", font=font_others, fill=255)
     draw.text((0, 24), f"SW01: {device_statuses['Switch']}", font=font_others, fill=255)
     draw.text((0, 36), f"AP01: {device_statuses['AP01']}", font=font_others, fill=255)
     draw.text((0, 48), f"AP02: {device_statuses['AP02']}", font=font_others, fill=255)
